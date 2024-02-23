17d1af8c01006842ca1e316de007771260c0d2cf
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-02-23 17:38:21 +0000

    Using urllib to request network resources.

--- netprog/mysocket.py
+++ netprog/mysocket.py
@@ -3,10 +3,12 @@ import socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.connect(("data.pr4e.org", 80))
 request = "GET http://data.pr4e.org/romeo-full.txt HTTP/1.0\r\n\r\n".encode()
+print("Request belongs to ", type(request))
 mysocket.send(request)
 
 while True:
     response = mysocket.recv(512)
+    print("Response is a ",type(response))
     if (len(response) < 1):
         break
     print(response.decode())
