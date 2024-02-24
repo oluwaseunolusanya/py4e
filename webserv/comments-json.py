33aeff5b1cfef0e056e5c976aa258cc72604a7d0
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-02-24 21:26:12 +0000

    Assigment on parsing JSON data.

--- webserv/comments-json.py
+++ webserv/comments-json.py
@@ -8,11 +8,12 @@ uh = urllib.request.urlopen(address)
 data = uh.read()
 print('Retrieved', len(data), 'characters')
 info = json.loads(data)
-print(info)
-
-# result = 0
-# for count in counts:
-#     result += int(count.text)
-
-# print("Count:",len(counts))
-# print("Sum:",result)
+# print(info)
+counts = list()
+comments = info["comments"]
+# print("Comments has ",len(comments), "elements")
+for i in range(len(comments)):
+    counts.append(comments[i]["count"])
+# print(counts)
+print("Count:",len(counts))
+print("Sum:",sum(counts))
