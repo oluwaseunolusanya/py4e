0b6b0b5526ea46fd1b4ad5fc170decdfb1f602be
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-02-28 19:18:31 +0000

    Database Relations.

---
+++ sql-db/tracksdb.py
@@ -0,0 +1,66 @@
+# App takes a csv files with tracks and create a database of Artists, Albums and Tracks.
+
+import sqlite3
+
+conn = sqlite3.connect("trackdb.sqlite3")
+cur = conn.cursor()
+
+# Make fresh tables.
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;  
+
+CREATE TABLE Artist (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id INTEGER PRIMARY KEY,
+    artist_id INTEGER,
+    title TEXT UNIQUE
+); 
+
+CREATE TABLE Track (
+    id INTEGER PRIMARY KEY,
+    title TEXT UNIQUE,
+    album_id INTEGER,
+    length INTEGER,
+    rating INTEGER,
+    count INTGER            
+);              
+''')
+fname = input("Enter the csv file of the tracks:")
+if len(fname) < 1:
+    fname = "tracks.csv"
+fhand = open(fname)
+
+for line in fhand:
+    line = line.strip()
+    pieces = line.split(",")
+    if len(pieces) < 6:
+        continue
+
+    name = pieces[0]
+    artist = pieces[1]
+    album = pieces[2]
+    count = pieces[3]
+    rating = pieces[4]
+    length = pieces[5]
+
+    print(name, artist, album, count, rating, length)
+
+    cur.execute("INSERT OR IGNORE INTO Artist(name) VALUES(?)",(artist,))
+    cur.execute("SELECT id FROM Artist WHERE name = ?",(artist,))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute("INSERT OR IGNORE INTO Album(title, artist_id) VALUES(?, ?)",(album, artist_id))
+    cur.execute("SELECT id FROM Album WHERE title = ?",(album,))
+    album_id = cur.fetchone()[0]
+
+    cur.execute("""INSERT OR REPLACE INTO Track(title, album_id, length, rating, count) 
+                   VALUES(?, ?, ?, ?, ?)""",(name, album_id, length, rating, count))
+    
+    conn.commit()
+    
